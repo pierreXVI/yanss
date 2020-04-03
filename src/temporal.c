c8cd571e0bea49e9c3f6acc2131bc89ccf11d487
Pierre Seize <pierreseize@gmail.com>
2020-04-03 16:04:21 +0200

    Writing user Viewer

--- src/temporal.c
+++ src/temporal.c
@@ -15,30 +15,30 @@ static PetscErrorCode TSMonitorDraw(TS ts, PetscInt steps, PetscReal time, Vec u
 
   PetscFunctionBeginUser;
 
-  PetscBool flg;
-  PetscInt  nmax = 2;
-  PetscReal vbound[nmax];
-  ierr = PetscOptionsGetRealArray(NULL, NULL, "-vec_view_bounds", vbound, &nmax, &flg); CHKERRQ(ierr);
-  if (!flg){
-    static PetscInt buffer_size = 32;
-    char            buffer[buffer_size];
-    ierr = VecMin(u, NULL, &vbound[0]);                                                 CHKERRQ(ierr);
-    ierr = VecMax(u, NULL, &vbound[1]);                                                 CHKERRQ(ierr);
-    if (vbound[1] <= vbound[0]) vbound[1] = vbound[0] + 1.0;
-    ierr = PetscSNPrintf(buffer, buffer_size, "%f,%f", vbound[0], vbound[1]);           CHKERRQ(ierr);
-    ierr = PetscOptionsSetValue(NULL, "-vec_view_bounds", buffer);                      CHKERRQ(ierr);
-  }
+  // PetscBool flg;
+  // PetscInt  nmax = 2;
+  // PetscReal vbound[nmax];
+  // ierr = PetscOptionsGetRealArray(NULL, NULL, "-vec_view_bounds", vbound, &nmax, &flg); CHKERRQ(ierr);
+  // if (!flg){
+  //   static PetscInt buffer_size = 32;
+  //   char            buffer[buffer_size];
+  //   ierr = VecMin(u, NULL, &vbound[0]);                                                 CHKERRQ(ierr);
+  //   ierr = VecMax(u, NULL, &vbound[1]);                                                 CHKERRQ(ierr);
+  //   if (vbound[1] <= vbound[0]) vbound[1] = vbound[0] + 1.0;
+  //   ierr = PetscSNPrintf(buffer, buffer_size, "%f,%f", vbound[0], vbound[1]);           CHKERRQ(ierr);
+  //   ierr = PetscOptionsSetValue(NULL, "-vec_view_bounds", buffer);                      CHKERRQ(ierr);
+  // }
 
   DM       dm;
   PetscInt numGhostCells;
-  ierr = VecGetDM(u, &dm);                                                              CHKERRQ(ierr);
-  ierr = HideGhostCells(dm, &numGhostCells);                                            CHKERRQ(ierr);
-  ierr = VecView(u, PETSC_VIEWER_DRAW_WORLD);                                           CHKERRQ(ierr);
-  ierr = RestoreGhostCells(dm, numGhostCells);                                          CHKERRQ(ierr);
+  ierr = VecGetDM(u, &dm);                     CHKERRQ(ierr);
+  ierr = HideGhostCells(dm, &numGhostCells);   CHKERRQ(ierr);
+  ierr = VecView(u, PETSC_VIEWER_DRAW_WORLD);  CHKERRQ(ierr);
+  ierr = RestoreGhostCells(dm, numGhostCells); CHKERRQ(ierr);
 
-  if (!flg){
-    ierr = PetscOptionsClearValue(NULL, "-vec_view_bounds");                            CHKERRQ(ierr);
-  }
+  // if (!flg){
+  //   ierr = PetscOptionsClearValue(NULL, "-vec_view_bounds");                            CHKERRQ(ierr);
+  // }
 
   PetscFunctionReturn(0);
 }
