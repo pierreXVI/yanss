877f8a280cdb66ae378915cb9520711f80d890b1
Pierre Seize <pierreseize@gmail.com>
2020-07-10 19:36:27 +0200

    Working periodic condition

--- include/utils.h
+++ include/utils.h
@@ -11,6 +11,7 @@
     BC_DIRICHLET: the conservative field values
     BC_OUTFLOW_P: the pressure value
     BC_WALL:      no value
+    BC_PERIO:     the translation
 */
 enum BCType {BC_DIRICHLET, BC_OUTFLOW_P, BC_WALL, BC_PERIO};
 
--- src/boundary.c
+++ src/boundary.c
@@ -62,13 +62,65 @@ PetscErrorCode BCWall(PetscReal time, const PetscReal c[3], const PetscReal n[3]
   PetscFunctionReturn(0);
 }
 
+
+#define ALMOST_EPS(a, b, c) PetscAbs(PetscAbs((a) - (b)) - (c)) < 1E-5
 PetscErrorCode BCPerio(PetscReal time, const PetscReal c[3], const PetscReal n[3], const PetscReal *xI, PetscReal *xG, void *ctx){
   struct BCCtx   *bc_ctx = (struct BCCtx*) ctx;
   PetscErrorCode ierr;
   DM dm;
 
   PetscFunctionBeginUser;
-  // ierr = DMView(bc_ctx->dm, PETSC_VIEWER_STDOUT_WORLD); CHKERRQ(ierr);
   for (PetscInt i = 0; i < bc_ctx->phys->dof; i++) xG[i] = 0;
+
+  // PetscPrintf(PETSC_COMM_WORLD, "In        %.2f, %.2f (% .2f, % .2f)\n", c[0], c[1], n[0], n[1]);
+
+  ierr = VecGetDM(bc_ctx->phys->x, &dm); CHKERRQ(ierr);
+
+  PetscInt fStart, fEnd, fMe=-1, fOther=-1;
+  PetscReal centroid[3], normal[3];
+  ierr = DMPlexGetHeightStratum(dm, 1, &fStart, &fEnd); CHKERRQ(ierr);
+  for (PetscInt f = fStart; f < fEnd; f++) {
+    DMPlexComputeCellGeometryFVM(dm, f, PETSC_NULL, centroid, normal); CHKERRQ(ierr);
+    if (centroid[0] == c[0] && centroid[1] == c[1]) {
+      fMe = f;
+    }
+    if ((ALMOST_EPS(centroid[0], c[0], 0.1) && ALMOST_EPS(centroid[1], c[1], 0)) || (ALMOST_EPS(centroid[1], c[1], 0.1) && ALMOST_EPS(centroid[0], c[0], 0))) { // TODO
+      fOther = f;
+      break;
+    }
+  }
+  // PetscPrintf(PETSC_COMM_WORLD, "    I am %d <- %d\n", fMe, fOther);
+
+  PetscInt p1, p2;
+  ierr = DMPlexGetGhostCellStratum(dm, &p1, &p2);  CHKERRQ(ierr);
+
+  PetscInt nC;
+  PetscInt const *support;
+  DMPlexGetSupportSize(dm, fOther, &nC);
+  DMPlexGetSupport(dm, fOther, &support);
+  // PetscPrintf(PETSC_COMM_WORLD, "    Support is of size %d\n", nC, support[0], support[1], p1, p2);
+
+  const PetscReal *ar;
+  PetscReal *xSym;
+  ierr = VecGetArrayRead(bc_ctx->phys->x, &ar); CHKERRQ(ierr);
+
+  for (PetscInt i = 0; i < nC; i++) {
+    DMPlexComputeCellGeometryFVM(dm, support[i], PETSC_NULL, centroid, normal); CHKERRQ(ierr);
+    if (support[i] < p2 && support[i] >= p1) {
+      // PetscPrintf(PETSC_COMM_WORLD, "    Cell %d is ghost\n", support[i]);
+    } else {
+      // PetscPrintf(PETSC_COMM_WORLD, "    Cell %d in %f, %f\n", support[i], centroid[0], centroid[1]);
+      ierr = DMPlexPointLocalRead(dm, support[i], ar, &xSym);CHKERRQ(ierr);
+      for (PetscInt i = 0; i < bc_ctx->phys->dof; i++) xG[i] = xSym[i];
+      break;
+    }
+
+  ierr = VecRestoreArrayRead(bc_ctx->phys->x, &ar); CHKERRQ(ierr);
+
+  }
+
+
+
+
   PetscFunctionReturn(0);
 }
